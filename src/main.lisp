3c16859e536939015d390f36ff33171e31ef7f79
selwyn <selwyn.simsek@cantab.net>
2025-02-15 16:59:33 +0000

    binary-transpose

--- src/main.lisp
+++ src/main.lisp
@@ -2,4 +2,15 @@
   (:use #:cl))
 (in-package #:binary-linear-algebra)
 
-;; blah blah blah.
+(defun binary-transpose (matrix)
+  "Returns the bt of a binary matrix."
+  (destructuring-bind (m n) (array-dimensions matrix)
+    (let ((result (bm0 n m)))
+      (loop for i from 0 below m
+            do (loop for j from 0 below n
+                     do (setf (aref result j i) (aref matrix i j))))
+      result)))
+
+(define-symbol-macro bt binary-transpose)
+
+(export '(bt binary-transpose))
